cbba9db49b1dc741f4aeb884463042808785d808
Alfonso de la Morena <alfonso.delamorena.engineer@gmail.com>
2017-12-28 19:57:24 -0600

    Update Bubble_Sort.cpp

--- Bubble Sort/Bubble_Sort.cpp
+++ Bubble Sort/Bubble_Sort.cpp
@@ -1,14 +1,14 @@
 /*
-    Binary Search
-
-    Date Posted: 10/06/2017
-    Language: C++
-    Files: 1 cpp file
-    Executable: Bubble_Sort.cpp
-    Libraries: iostream, cstdlib
-    Largest Run Time <= O(N^2)
-
-*/
+ *   Binary Search
+ *
+ *   Date Posted: 10/06/2017
+ *   Language: C++
+ *   Files: 1 cpp file
+ *   Executable: Bubble_Sort.cpp
+ *   Libraries: iostream, cstdlib
+ *   Largest Run Time <= O(N^2)
+ *
+ */
 
 #include <cstdlib> //For system("PAUSE>NUL"); (optional)
 #include<iostream>
