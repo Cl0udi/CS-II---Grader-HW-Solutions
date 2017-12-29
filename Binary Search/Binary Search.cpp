a2e453e83bd5d587815191539b5b83bf5ab7029d
Alfonso de la Morena <alfonso.delamorena.engineer@gmail.com>
2017-12-28 19:56:38 -0600

    Update Binary Search.cpp

--- Binary Search/Binary Search.cpp
+++ Binary Search/Binary Search.cpp
@@ -1,14 +1,14 @@
 /*
-    Binary Search
-    
-    Date Posted: 09/29/2017
-    Language: C++
-    Files: 1 cpp file
-    Executable: Binary_Search.cpp
-    Libraries: iostream, cstdlib
-    Largest Run Time <= O(log(N))
-
-*/
+ *    Binary Search
+ *    
+ *    Date Posted: 09/29/2017
+ *    Language: C++
+ *    Files: 1 cpp file
+ *    Executable: Binary_Search.cpp
+ *    Libraries: iostream, cstdlib
+ *    Largest Run Time <= O(log(N))
+ *
+ */
 
 #include <cstdlib> //For system("PAUSE>NUL"); (optional)
 #include<iostream>
